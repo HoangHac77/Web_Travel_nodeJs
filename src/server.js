67f73caddd09117bfac0696109123a4947388b5f
Hoang Hac <khongcono92@example.com>
2023-03-20 19:23:49 +0700

    Create project, build structure project

---
+++ src/server.js
@@ -0,0 +1,11 @@
+// library
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+app.listen(PORT, ()=>{
+  console.log(`Server running at Port:`, PORT)
+})
+
+
