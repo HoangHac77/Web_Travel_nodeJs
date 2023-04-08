9cdc6a97af96ed9406754199f329e23dab1d852e
DungNguyen <quocdung.nguyen1207@gmail.com>
2023-04-08 19:05:13 +0700

    XDWATDL-10 XDWATDL-22 page user

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,21 @@ import HomePage from "./pages/home/index"
 
 function App() {
   return (
-    <>
-      <HomePage/>
-    </>
+
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<HomePages.MasterLayout />}>
+          <Route path="" element={<HomePages.Home />} />
+          <Route path="contact" element={<HomePages.Contact />} />
+          <Route path="about" element={<HomePages.About />} />
+          <Route path="tours" element={<HomePages.Tours />} />
+          <Route path="tours/details/:id" element={<HomePages.DetailTour />} />
+        </Route>
+        <Route path="/login" exact element={<AuthPage.Login />} />
+        <Route path="/signup" exact element={<AuthPage.Register />} />
+      </Routes>
+    </Router>
+
   );
 }
 
